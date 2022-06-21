2ac2e49a213f8a4335bc47a2ad26c2333b84b02a
Oliver Krumbæk Perving <Ollikp@gmail.com>
2022-06-21 14:41:52 +0200

    har fjernet ubruelig javascript

--- javascript/main.js
+++ javascript/main.js
@@ -1,23 +1,3 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function showBugerMenu() {
-    document.getElementById("dropdownMenu").classList.toggle("show");
-  }
-  
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.burger-menu')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }  
-}
-
 //Read more funktion på FAQ siden, til hvert spørgsmål//
 //Sektion med Praktiske spørgsmål//
 function disabledFriendlyFunction() {
--- javascript/vaerelser.js
+++ javascript/vaerelser.js
@@ -27,7 +27,6 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
 }
 
 /* Her slutter JavaScript koden fra w3school*/
